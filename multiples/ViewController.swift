5c7be28f9f4a331272a64f61dfbaef575baf9f46
Vikas <vikcher123@icloud.com>
2016-01-12 22:57:00 -0500

    First commit - multiples app

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -9,15 +9,82 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Properties
+    
+    var startingSum:Int = 0
+    var rollingSum:Int = 0
+    var addingFactor:Int = 0
+    var beforeRollingSum:Int = 0
+    let maxSum:Int = 50
+    
+    //Outlets
+    
+    @IBOutlet weak var playBtn:UIButton!
+    @IBOutlet weak var addBtn:UIButton!
+    @IBOutlet weak var textBox:UITextField!
+    @IBOutlet weak var addLabel:UILabel!
+    @IBOutlet weak var header:UIImageView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Actions
+    
+    @IBAction func playButtonPressed (sender: UIButton) {
+        if (textBox.text != nil && textBox.text != "") {
+            addingFactor = Int(textBox.text!)!
+            startGame()
+        }
+    }
+    
+    func startGame() {
+        playBtn.hidden = true
+        header.hidden = true
+        textBox.hidden = true
+        
+        addBtn.hidden = false
+        addLabel.hidden = false
+    }
+    
+    func restartGame() {
+        playBtn.hidden = false
+        header.hidden = false
+        textBox.hidden = false
+        
+        addBtn.hidden = true
+        addLabel.text = "Start"
+        addLabel.hidden = true
+        startingSum = 0
+        rollingSum = 0
+        addingFactor = 0
+    }
+    
+    @IBAction func addButtonPressed (sender: UIButton) {
+        beforeRollingSum = rollingSum
+        rollingSum = beforeRollingSum + addingFactor
+        
+        if (rollingSum >= maxSum) {
+            restartGame()
+        } else {
+            addLabel.text = "\(beforeRollingSum) + \(addingFactor) = \(rollingSum)"
+        }
     }
 
 
